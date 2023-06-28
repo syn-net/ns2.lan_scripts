6c7c2ff5d47b9a21bad976335d93cfc529540516
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2023-06-28 18:30:00 -0500

    Add parsing of configuration file

--- bin/toggle-smart-plug-pwr.sh
+++ bin/toggle-smart-plug-pwr.sh
@@ -11,13 +11,45 @@ run() {
   fi
 }
 
-CURL_CMD=$(command -v curl)
-PING_CMD=$(command -v ping)
+load_config() {
+  SCRIPT_BASE="$(dirname $0)"
+
+  if [ -f "${SCRIPT_BASE}/.env.development" ] || [ -L "${SCRIPT_BASE}/.env.development" ]; then
+    echo "Loading configuration from ${SCRIPT_BASE}/.env.development..."
+    source "${SCRIPT_BASE}/.env.development"
+  elif [ -f "${SCRIPT_BASE}/.env" ] || [ -L "${SCRIPT_BASE}/.env" ]; then
+    echo "Loading configuration from ${SCRIPT_BASE}/.env..."
+    source "${SCRIPT_BASE}/.env"
+  else
+    echo "Loading configuration from ${SCRIPT_BASE}/.env.dist..."
+    source "${SCRIPT_BASE}/.env.dist"
+  fi
+}
+
+CURL_CMD=$(which curl)
+PING_CMD=$(which ping)
 PING_ARGS="-4 -w1"
 CURL_ARGS="-4"
-HOST=$1
-PWR_SWITCH_POS=$2
+HOST="$1"
+PWR_SWITCH_POS="$2"
 
+load_config
+
+# Pre-run sanity check
+if [ ! -x "$CURL_CMD" ]; then
+  echo "CRITICAL: Could not find curl at ${CURL_CMD}."
+  echo
+  exit 2
+fi
+
+# Pre-run sanity check
+if [ ! -x "$PING_CMD" ]; then
+  echo "CRITICAL: Could not find ping at ${PING_CMD}."
+  echo
+  exit 2
+fi
+
+# obligatory usage text
 if [ -z "$HOST" ]; then
   echo "USAGE: This script requires one argument."
   echo "    <hostname>... an IP or hostname to connect to"
@@ -31,43 +63,40 @@ if [ -z "$HOST" ]; then
   exit 255
 fi
 
+# Defaults
 if [ -z "$PWR_SWITCH_POS" ]; then
   PWR_SWITCH_POS="query"
 fi
 
+# Pre-run check
+if ! run "${PING_CMD}" ${PING_ARGS} "${HOST}" > /dev/null; then
+  echo "CRITICAL: The host at ${HOST} appears to be offline. Try again later!"
+  echo
+  exit 1
+fi
+
+# First branch
 if [ "$PWR_SWITCH_POS" = "query" ]; then
   echo
   # shellcheck disable=SC2086
-  run "$PING_CMD" $PING_ARGS "$HOST" && "$CURL_CMD" "$CURL_ARGS" \
-    "http://$HOST/cm?cmnd=Power&user=admin&password=19841985"
+  "${CURL_CMD}" "${CURL_ARGS}" "http://$HOST/cm?cmnd=Power&user=${USERNAME}&password=${PASSWORD}"
   echo
   exit 0
 fi
 
-
+# Second and our last branch
 if [ "$PWR_SWITCH_POS" = "toggle" ]; then
   # shellcheck disable=SC2086
   # shellcheck disable=SC2091
-  if ! $(run "$PING_CMD" $PING_ARGS "$HOST" && "$CURL_CMD" "$CURL_ARGS" "http://$HOST/cm?cmnd=Power&user=admin&password=19841985" | grep -i -e "ON"); then
+  if ! $(run "$CURL_CMD" "$CURL_ARGS" "http://$HOST/cm?cmnd=Power&user=${USERNAME}&password=${PASSWORD}" | grep -i -e "ON"); then
     PWR_SWITCH_POS="OFF"
   else
     PWR_SWITCH_POS="ON"
   fi
 fi
 
-if [ ! -x "$CURL_CMD" ]; then
-  echo "CRITICAL: Could not find curl at ${CURL_CMD}."
-  echo
-  exit 2
-fi
-
-if [ ! -x "$PING_CMD" ]; then
-  echo "CRITICAL: Could not find ping at ${PING_CMD}."
-  echo
-  exit 2
-fi
-
 # shellcheck disable=SC2086
-run "$PING_CMD" $PING_ARGS "$HOST" && "$CURL_CMD" "$CURL_ARGS" "http://$HOST/cm?cmnd=Power%20${PWR_SWITCH_POS}&user=admin&password=19841985"
+run "$PING_CMD" $PING_ARGS "$HOST" && "$CURL_CMD" "$CURL_ARGS" \
+  "http://${HOST}/cm?cmnd=Power%20${PWR_SWITCH_POS}&user=${USERNAME}&password=${PASSWORD}"
 
 exit 0
